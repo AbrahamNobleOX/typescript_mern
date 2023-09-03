867149b29addc16f1ac1c0f10fc32c4eff45115d
Akintola Abraham <abramseyakid@gmail.com>
2023-09-03 22:58:55 +0100

    added create deck frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import cors from "cors";
+import { createDeckController } from "./controllers/createDeckController";
 
 const app = express();
 config();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
+
+app.post("/decks", createDeckController);
 
 const PORT = process.env.PORT || 8000;
 
