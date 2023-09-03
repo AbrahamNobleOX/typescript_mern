c79b5e6d5ca2bfde485f9b6c506fcde6e6a6ba6f
Akintola Abraham <abramseyakid@gmail.com>
2023-09-04 00:35:03 +0100

    added delete deck

--- server/src/controllers/deleteDeckController.ts
+++ server/src/controllers/deleteDeckController.ts
@@ -1,1 +1,8 @@
+import { Request, Response } from "express";
+import Deck from "../models/Deck";
 
+export async function deleteDeckController(req: Request, res: Response) {
+  const deckId = req.params.deckId;
+  const deck = await Deck.findByIdAndDelete(deckId);
+  res.status(200).json(deck);
+}
