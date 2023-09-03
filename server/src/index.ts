522f4e474d96f91c9e1c31ae16f11ac19df0ff47
Akintola Abraham <abramseyakid@gmail.com>
2023-09-03 21:16:55 +0100

    added create deck

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,19 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
 const app = express();
+config();
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
+});
